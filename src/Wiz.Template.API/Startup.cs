1ec6975872517e719a8a3d964ce9428985b8eb32
Juscélio Reis <juscelioreis@wizsolucoes.com.br>
2021-02-24 14:47:22 -0300

    fix: return syncronous io

--- src/Wiz.Template.API/Startup.cs
+++ src/Wiz.Template.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
