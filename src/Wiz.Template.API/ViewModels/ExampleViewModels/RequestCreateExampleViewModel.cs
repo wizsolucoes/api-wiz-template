de722570d648da85e0280164ba4b011299b7cf46
bernas1104 <bernardoc1104@gmail.com>
2022-03-08 18:20:47 -0300

    chore(repository): documentação view model de requisição de criação

--- src/Wiz.Template.API/ViewModels/ExampleViewModels/RequestCreateExampleViewModel.cs
+++ src/Wiz.Template.API/ViewModels/ExampleViewModels/RequestCreateExampleViewModel.cs
@@ -6,7 +6,15 @@ namespace Wiz.Template.API.ViewModels.ExampleViewModels
 {
     public record RequestCreateExampleViewModel : IRequest<Option<Example>>
     {
+        /// <summary>
+        /// Temperatura em Celsius
+        /// </summary>
+        /// <example>25</example>
         public int TemperatureC { get; init; }
+        /// <summary>
+        /// Resumo da sensação do dia
+        /// </summary>
+        /// <example>Mild</example>
         public string Summary { get; init; }
     }
 }
